2178e11d24e22ab800b34613d5dbfa207fe5d06d
Manvendra2509 <manvendra.17cse468@rtu.ac.in>
2023-06-02 17:05:25 +0530

    Changed variable types

--- calculator.js
+++ calculator.js
@@ -20,8 +20,8 @@ app.get("/bmiCalculator", function (req, res) {
 });
 
 app.post("/bmiCalculator", function (req, res) {
-    var weight = Number(req.body.weight);
-    var height = Number(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
     var bmi = weight / (height*height);
     res.send("Your BMI is " + bmi);
 });
